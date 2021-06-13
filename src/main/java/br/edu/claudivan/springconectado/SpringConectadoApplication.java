55add9988f4f5bb81564716097fcee2943fb6a0b
Claudivan Moreira <claudivan.ifpb@gmail.com>
2021-06-13 12:34:47 -0300

    Initial commit

---
+++ src/main/java/br/edu/claudivan/springconectado/SpringConectadoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.claudivan.springconectado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConectadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConectadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/claudivan/springconectado/SpringConectadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.claudivan.springconectado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConectadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
